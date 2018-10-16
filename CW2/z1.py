8506b05e60504222ba3a46b7fc87b98d2f43818d
Reznot <Reznot24@gmail.com>
2018-10-16 13:34:37 +0200

    Initial commit

---
+++ CW2/z1.py
@@ -0,0 +1,41 @@
+import re
+
+name = input('Podaj imie ')
+
+name_Check = re.match(r'[A-Z][a-z]+',name)
+if name_Check:
+    print(name_Check.group())
+else:
+    print('Blad')
+    exit(1)
+
+
+surname = input('Podaj nazwisko ')
+check_Surname = re.match(r'[A-Z][a-z]+',surname)
+
+if check_Surname is None:
+    print('Blad')
+    exit(1)
+
+
+city = input('Podaj miasto ')
+check_City = re.match(r'[A-Z][a-z]+',city)
+
+if check_City is None:
+    print('Blad')
+    exit(1)
+
+phone = input('Podaj numer tel')
+check_phone = re.match(r'\(\d\d\) \d\d\d-\d\d-\d\d$', phone)
+if check_phone is None:
+    print('Blad')
+    exit(1)
+
+
+postcode = input('Podaj kod pocztowy')
+check_Postcode = re.match(r'\d\d-\d\d\d$',postcode)
+if check_Postcode is None:
+    print('Blad')
+    exit(1)
+
+#moge napisywac matchingObj
---
+++ CW2/z2.py
@@ -0,0 +1,8 @@
+import re
+
+with open('file.txt') as in_file:
+    for line in in_file:
+        string1 = line.rstrip()
+
+
+line_Check = re.match(r'', string1)
