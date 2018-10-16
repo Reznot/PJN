c5753fcfbdc54a4e2ac9c46d43320d6e4d410374
Reznot <Reznot24@gmail.com>
2018-10-16 14:10:04 +0200

    Minor improvements

--- CW2/z2.py
+++ CW2/z2.py
@@ -1,14 +1,11 @@
 import re
 
-with open('file.txt') as in_file:
+
+with open('file2.csv') as in_file:
+    reader = csv.reader(in_file)
     for line in in_file:
-        string1 = line.rstrip()
+        CSVstring = re.search(r'.-\d-\d*$', line)
 
 
 line_Check = re.match(r'', string1)
 
-#
-# with open('file2.csv') as in_file:
-#     reader = csv.reader(in_file)
-#     for line in in_file:
-#         CSVstring = re.search(r'.-\d-\d*$', line)
