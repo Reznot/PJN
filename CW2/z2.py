8cd39b2e7f240fd92f6addc494f7b496d8bb2462
Reznot <Reznot24@gmail.com>
2018-10-19 17:43:57 +0200

    Assignement 2 completed

--- CW2/z2.py
+++ CW2/z2.py
@@ -1,11 +1,25 @@
 import re
+import os
+
+# cwd = os.getcwd()
+# print(cwd)
 
 
-with open('file2.csv') as in_file:
-    reader = csv.reader(in_file)
-    for line in in_file:
-        CSVstring = re.search(r'.-\d-\d*$', line)
+def isCSVFileValid(obj):
+    if obj is None:
+        print("This file is not valid.")
+        exit(1)
+    else:
+        print("This file is valid")
+        exit(0)
 
 
-line_Check = re.match(r'', string1)
+filename = input("Enter name of the file to validate")
+file = open(filename, 'r')
+
+for line in file:
+    isMatchingObj = re.match(r'.*;\d*;\d*', line)
+    isCSVFileValid(isMatchingObj)
+file.close()
+
 
