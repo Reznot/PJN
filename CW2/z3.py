42fadb7442feaaf04973c8b6d031c434a37e0e8a
Reznot <Reznot24@gmail.com>
2018-10-19 18:40:02 +0200

    Assignement 3 completed

--- CW2/z3.py
+++ CW2/z3.py
@@ -1,14 +1,15 @@
 import re
 
-dirtyfilename = input("Type the filename to clean (f.e. file.txt)")
-cleanfilename = (dirtyfilename.split("."))[0]+"CLEAN."+(dirtyfilename.split("."))[1]
+explicitfilename = input("Enter the name of a file to be censored: ")
+censoredfilename = (explicitfilename.split("."))[0] + "CENSORED." + (explicitfilename.split("."))[1]
 
-dirty = open(dirtyfilename, 'r+')
-clean = open(cleanfilename, 'w+')
+explicit = open(explicitfilename, 'r')
+censored = open(censoredfilename, 'w+')
 
-for line in dirty:
-    clean.write(re.sub(r'\w*[Kk]urw\w*|\b[Cc]huj\w*|^[Cc]ip\w*|[Pp]izd\w*|\w*[Jj]eb\w*|\w*[Pp]ierdol\w*', '---', line))
+for line in explicit:
+    censored.write(re.sub(r'\w*[Kk]urw\w*|\w*[Jj]eb\w*|\w*[Pp]ierdol\w*|\w*[Cc]huj\w*|^[Cc]ip\w*|[Pp]izd\w*', '---', line))
 
-print("Censored file saved in "+ cleanfilename)
-dirty.close()
-clean.close()
+
+print("Text has been censored")
+explicit.close()
+censored.close()
