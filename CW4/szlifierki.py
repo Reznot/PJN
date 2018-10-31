a9a06d7998dab16410e1f67b4f78a6286d30ba55
Reznot <Reznot24@gmail.com>
2018-10-31 15:11:25 +0100

    scraping in loop added

--- CW4/szlifierki.py
+++ CW4/szlifierki.py
@@ -7,8 +7,12 @@ sources = ['https://www.ceneo.pl/Narzedzia_i_warsztat;szukaj-szlifierka+k%C4%85t
            'https://www.ceneo.pl/Narzedzia_i_warsztat;szukaj-szlifierka+k%C4%85towa;0020-30-0-0-2.htm', 'https://www.ceneo.pl/Narzedzia_i_warsztat;szukaj-szlifierka+k%C4%85towa;0020-30-0-0-3.htm',
            'https://www.ceneo.pl/Narzedzia_i_warsztat;szukaj-szlifierka+k%C4%85towa;0020-30-0-0-4.htm']
 
-for i in range(len(sources)):
-    response = urllib.request.urlopen(sources[i])
+# for i in range(len(sources)):
+    # response = urllib.request.urlopen(sources[i])
+    # page_source = response.read()
+    # grinders += re.findall(r'data-source-tag="">([A-Za-z0-9 ]+)</a>', str(page_source))
+for i in range(50):
+    response = urllib.request.urlopen("https://www.ceneo.pl/Narzedzia_i_warsztat;szukaj-szlifierka+k%C4%85towa;0020-30-0-0-{}.htm".format(i))
     page_source = response.read()
     grinders += re.findall(r'data-source-tag="">([A-Za-z0-9 ]+)</a>', str(page_source))
 
